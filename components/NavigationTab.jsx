a9b7c88990863cfcc3c666c6f6a45e994cea17e9
Nathania Calista <nathania.calista01@gmail.com>
2024-05-05 07:02:24 +0700

    fix : navigation bar and landing screen

--- components/NavigationTab.jsx
+++ components/NavigationTab.jsx
@@ -5,6 +5,7 @@ import CalendarScreen from "../screens/CalendarScreen";
 import ForumScreen from "../screens/ForumScreen";
 import StatisticsScreen from "../screens/StatisticsScreen";
 import ProfileScreen from "../screens/ProfileScreen";
+import { Image } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
@@ -15,11 +16,66 @@ const NavigationTab = () => {
         headerShown: false, // This will hide headers for all tab screens globally
       }}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Calendar" component={CalendarScreen} />
-      <Tab.Screen name="Forum" component={ForumScreen} />
-      <Tab.Screen name="Statistics" component={StatisticsScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../assets/navigations/home.png")}
+              style={{ width: size, height: size, tintColor: color }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={CalendarScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../assets/navigations/calendar.png")}
+              style={{ width: size, height: size, tintColor: color }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Forum"
+        component={ForumScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../assets/navigations/forum.png")}
+              style={{ width: size, height: size, tintColor: color }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Statistics"
+        component={StatisticsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../assets/navigations/statistics.png")}
+              style={{ width: size, height: size, tintColor: color }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../assets/navigations/profile.png")}
+              style={{ width: size, height: size, tintColor: color }}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
--- screens/LandingScreen.jsx
+++ screens/LandingScreen.jsx
@@ -8,56 +8,72 @@ import {
   View,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useFonts } from "expo-font";
 
 const LandingScreen = ({ navigation }) => {
+  const [fontsLoaded] = useFonts({
+    Karla: require("../assets/fonts/Karla.ttf"),
+  });
   return (
-    <View>
-      <LinearGradient
-        colors={["#FDE9EC", "#FFFFFF", "#C4F5EA"]}
-        className="absolute h-screen w-screen flex items-center justify-center"
-      >
-        <View className="absolute top-[15%] flex flex-col w-full h-screen items-center justify-start">
-          <Image
-            source={require("../assets/logo.png")}
-            resizeMode="contain"
-            className="h-1/3 mb-10"
-          />
-          <Text className="text-[20px] font-medium text-[#3C548D] leading-[28px] tracking-wider">
-            Welcome to
-          </Text>
-          <Image
-            source={require("../assets/image.png")}
-            resizeMode="contain"
-            className="h-10 mt-4"
-          />
-          <TouchableOpacity onPress={() => navigation.navigate("MainApp")}>
-            <LinearGradient
-              colors={["#87DCCB", "#D6BDFF"]}
-              start={{ x: 0, y: 0 }}
-              end={{ x: 1, y: 0 }}
-              className="w-[320px] p-[12px] rounded-3xl mt-12"
-              onPress={() => navigation.replace("Home")}
+    <LinearGradient
+      colors={["#FDE9EC", "#FFFFFF", "#C4F5EA"]}
+      className="absolute h-full w-screen flex items-center justify-center"
+    >
+      <View className="top-[15%] flex flex-col w-full h-screen items-center justify-start">
+        <Image
+          source={require("../assets/logo.png")}
+          resizeMode="contain"
+          className="h-1/3 mb-10"
+        />
+        <Text
+          className="text-[20px] font-medium text-[#3C548D] leading-[28px] tracking-wider"
+          style={{ fontFamily: "Karla" }}
+        >
+          Welcome to
+        </Text>
+        <Image
+          source={require("../assets/image.png")}
+          resizeMode="contain"
+          className="h-10 mt-4"
+        />
+        <TouchableOpacity onPress={() => navigation.navigate("MainApp")}>
+          <LinearGradient
+            colors={["#87DCCB", "#D6BDFF"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            className="w-[320px] p-[12px] rounded-3xl mt-12"
+            onPress={() => navigation.replace("Home")}
+          >
+            <Text
+              className="text-white text-center text-[16px] font-bold"
+              style={{ fontFamily: "Karla" }}
             >
-              <Text className="text-white text-center text-[16px] font-bold">
-                Sign up
-              </Text>
-            </LinearGradient>
-          </TouchableOpacity>
+              Sign up
+            </Text>
+          </LinearGradient>
+        </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => navigation.replace("Home")}>
-            <View className="w-[320px] p-[12px] bg-twhite border-[1px] border-[#D6BDFF] rounded-3xl mt-4">
-              <Text className="text-white text-center text-[16px] font-bold text-[#3C548D]">
-                Sign In with Google
-              </Text>
-            </View>
-          </TouchableOpacity>
-
-          <View className="w-[320px] mt-6">
-            <Text className="text-[#3C548D] text-center">Login</Text>
+        <TouchableOpacity onPress={() => navigation.replace("Home")}>
+          <View className="w-[320px] p-[12px] bg-twhite border-[1px] border-[#D6BDFF] rounded-3xl mt-4">
+            <Text
+              className="text-white text-center text-[16px] font-bold text-[#3C548D]"
+              style={{ fontFamily: "Karla" }}
+            >
+              Sign In with Google
+            </Text>
           </View>
+        </TouchableOpacity>
+
+        <View className="w-[320px] mt-6">
+          <Text
+            className="text-[#3C548D] text-center"
+            style={{ fontFamily: "Karla" }}
+          >
+            Login
+          </Text>
         </View>
-      </LinearGradient>
-    </View>
+      </View>
+    </LinearGradient>
   );
 };
 export default LandingScreen;
